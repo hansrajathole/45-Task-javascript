6feae9e8fc53108e6de1ccebaa201a55e18385e6
atholehansraj <hansrajathole1@gmail.com>
2025-01-05 23:07:19 +0530

    send request-setTimeout done

---
+++ Send-Request-setTimeout/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div  className='h-screen w-full '>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
